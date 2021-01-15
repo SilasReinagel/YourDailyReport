a3a202073bac60365925d15afee0c92913c460f8
Silas Reinagel <silas.reinagel@carvana.com>
2021-01-15 16:54:24 -0700

    Initial Project Setup. Added basic Email Sender.

---
+++ src/Functions/MailSender.cs
@@ -0,0 +1,46 @@
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace SilasReinagel.YourDailyReport
+{
+  public class MailSender
+  {
+      private readonly MailSenderConfigString _cfg;
+
+      public MailSender(MailSenderConfigString cfg) => _cfg = cfg;
+
+      public async Task Send(string toAddress, string subject, string body)
+      {
+         var message = new MailMessage();  
+         var smtp = new SmtpClient();  
+         message.From = new MailAddress(_cfg.From);  
+         message.To.Add(new MailAddress(toAddress));  
+         message.Subject = subject;
+         message.IsBodyHtml = false;
+         message.Body = body;
+         
+         smtp.Port = _cfg.SmtpPort;
+         smtp.Host = _cfg.SmtpHost; 
+         smtp.EnableSsl = true;  
+         smtp.UseDefaultCredentials = false;
+         smtp.Credentials = new NetworkCredential(_cfg.Username, _cfg.Password);  
+         smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+         
+         await smtp.SendMailAsync(message);
+      }
+  }
+
+  public class MailSenderConfigString
+  {
+      private readonly string _cfg;
+
+      public MailSenderConfigString(string cfg) => _cfg = cfg;
+
+      public string SmtpHost => _cfg.Split('|')[0].Split(':')[0];
+      public int SmtpPort => int.Parse(_cfg.Split('|')[0].Split(':')[1]);
+      public string From => _cfg.Split('|')[1];
+      public string Username => _cfg.Split('|')[2];
+      public string Password => _cfg.Split('|')[3];
+  }
+}
---
+++ src/Tests/IntegrationTests.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SilasReinagel.YourDailyReport;
+
+namespace Tests
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        [TestMethod]
+        public async Task Email_TestSend()
+        {
+            var email = new MailSender(new MailSenderConfigString(Environment.GetEnvironmentVariable("YourDailyReportMailConfig")));
+            
+            await email.Send("silas.reinagel@gmail.com", "Test Report 2 - 01/15/21", "Test Data\r\nLine 2");
+        }
+    }
+}
